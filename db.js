131e960a827d979d73c20a48e61cd9f70244ae6f
Satyam <karnsaty69@gmail.com>
2025-03-26 16:53:24 +0545

    db.js

--- db.js
+++ db.js
@@ -8,10 +8,10 @@ const password = encodeURIComponent('s3ZZWypLJCkccFqV');
 
 export const connectDB = async () => {
   try {
-      // const client = new MongoClient(`mongodb+srv://${userName}:${password}@cluster0.hfsa6.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
-      //    ); 
+      const client = new MongoClient(`mongodb+srv://${userName}:${password}@cluster0.hfsa6.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
+         ); 
 
-   const client = new MongoClient('mongodb://localhost:27017');
+  //  const client = new MongoClient('mongodb://localhost:27017');
     await client.connect();
     console.log('Connected to MongoDB! !!!!!');
     db_1 = client.db(dbName);
